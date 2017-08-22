9cd11413c70f0dd27b921d3067d87d24f633f0cc
Adrian DC <radian.dc@gmail.com>
2017-08-22 11:57:09 +0200

    camera: Upgrade library dependencies for AOSP 8.0.0
    Change-Id: If04e62bbf0905534c03438f95458f0d6c5045ae2
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- camera/Android.mk
+++ camera/Android.mk
@@ -7,10 +7,21 @@ LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
 LOCAL_C_INCLUDES := \
+    framework/native/include \
     system/media/camera/include
 
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libutils libcutils
+    libhardware \
+    liblog \
+    libcamera_client \
+    libgui \
+    libhidltransport \
+    libsensor \
+    libutils \
+    android.hidl.token@1.0-utils
+
+LOCAL_STATIC_LIBRARIES := \
+    libarect
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/hw
 LOCAL_MODULE := camera.qcom
